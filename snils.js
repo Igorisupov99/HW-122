1dfb3ea2e4c26f78ad17bb71dd64f1ea0363c715
AlekseyLexey <axcerz@gmail.com>
2025-02-26 06:15:40 +1000

    create: formatSNILS

--- snils.js
+++ snils.js
@@ -1,43 +1,42 @@
 // Изучи тесты `spec/snils.spec.js`
 // Чтобы понимать как должны работать твои методы.
 
-
 // Определи, есть ли во входной строке номер СНИЛС.
 function hasSNILS(string) {
-    return /\d{3}-[0-9]{3}-[0-9]{3} [0-9]{2}/.test(string)
+  return /\d{3}-[0-9]{3}-[0-9]{3} [0-9]{2}/.test(string);
 }
-
 
 // Найди и верни номер СНИЛС из строки.
 function grabSNILS(string) {
-    const str = string.match(/\d{3}-[0-9]{3}-[0-9]{3} [0-9]{2}/).join('')
-    return str ? str : ''
+  const str = string.match(/\d{3}-[0-9]{3}-[0-9]{3} [0-9]{2}/).join("");
+  return str ? str : "";
 }
-
 
 // Найди и верни все номера СНИЛС, что есть в строке.
 function grabAllSNILS(string) {
-return string.match(/\d{3}-[0-9]{3}-[0-9]{3} [0-9]{2}/g)
+  return string.match(/\d{3}-[0-9]{3}-[0-9]{3} [0-9]{2}/g);
 }
-
 
 // Зашифруй номера СНИЛС. Example: XXX-XXX-XXX 30.
 function hideAllSNILS(string) {
-return string.replace(/\d{3}-[0-9]{3}-[0-9]{3}/g, 'XXX-XXX-XXX')
+  return string.replace(/\d{3}-[0-9]{3}-[0-9]{3}/g, "XXX-XXX-XXX");
 }
-
-
 
 // Отформатируй все номера СНИЛС, чтобы использовались правильные разделители:
 // '48001443027', '480.014.430.27', и '480--014--430 27' должны превратиться в '480-014-430 27'.
 function formatSNILS(string) {
-
+  return string.replace(
+    /(\d{3})[.\s-]*(\d{3})[.\s-]*(\d{3})[.\s-]*(\d{2})/g,
+    (match, p1, p2, p3, p4) => {
+      return `${p1}-${p2}-${p3} ${p4}`;
+    }
+  );
 }
 
 module.exports = {
-    hasSNILS,
-    grabSNILS,
-    grabAllSNILS,
-    hideAllSNILS,
-    formatSNILS
+  hasSNILS,
+  grabSNILS,
+  grabAllSNILS,
+  hideAllSNILS,
+  formatSNILS,
 };
